57192db21eedf3c05ae74c3b692e7229ca19c802
Itay Lotan <itayl@diagnosticrobotics.com>
2019-12-23 19:19:40 +0200

    more improvements

--- main.rb
+++ main.rb
@@ -15,13 +15,15 @@ def main
 
     branch_name =
         if selected_branch.is_remote?
-            # Let get to the trick - it will create a local branch for the remote one for us
+            # Let git to the trick - it will create a local branch for the remote one for us
             remove_remote_prefix(selected_branch.name)
         else
             selected_branch.name
         end
 
-    post_checkout_operations = ask_user_which_post_checkout_operations_to_perform(selected_branch)
+    post_checkout_operations = ask_user_which_post_checkout_operations_to_perform()
+    should_run_update_sh = ask_user_if_should_run_main_update_sh_script()
+    post_checkout_operations[:should_run_update_sh] = should_run_update_sh
 
     smart_checkout(branch_name, post_checkout_operations)
 end
@@ -53,20 +55,20 @@ def ask_user_for_selected_branch(local_branches, remote_branches, current_branch
     selected_branch
 end
 
-def ask_user_which_post_checkout_operations_to_perform(selected_branch)
-    if selected_branch.is_remote?
-        { perform_pull: false, perform_migrations: false }
-    else
-        PROMPT.select('do you want to pull / run migrations as well?', [
-            { name: 'do nothing', value: { perform_pull: false, perform_migrations: false, perform_rebase_onto_origin_master: false } },
-            { name: 'pull and run migrations', value: { perform_pull: true, perform_migrations: true, perform_rebase_onto_origin_master: false } },
-            { name: 'pull, run migrations and rebase onto origin/master', value: { perform_pull: true, perform_migrations: true, perform_rebase_onto_origin_master: true } },
-            { name: "pull and rebase onto origin/master", value: { perform_pull: true, perform_migrations: false, perform_rebase_onto_origin_master: true } },
-            { name: "rebase onto origin/master only", value: { perform_pull: false, perform_migrations: false, perform_rebase_onto_origin_master: true } },
-            { name: "pull only", value: { perform_pull: true, perform_migrations: false, perform_rebase_onto_origin_master: false } },
-            { name: "run migrations only", value: { perform_pull: false, perform_migrations: true, perform_rebase_onto_origin_master: false } }
-        ])
-    end
+def ask_user_which_post_checkout_operations_to_perform()
+    PROMPT.select('do you want to pull / run migrations as well?', [
+        { name: 'do nothing', value: { perform_pull: false, perform_migrations: false, perform_rebase_onto_origin_master: false } },
+        { name: 'pull and run migrations', value: { perform_pull: true, perform_migrations: true, perform_rebase_onto_origin_master: false } },
+        { name: "pull only", value: { perform_pull: true, perform_migrations: false, perform_rebase_onto_origin_master: false } },
+        { name: 'pull, run migrations and rebase onto origin/master', value: { perform_pull: true, perform_migrations: true, perform_rebase_onto_origin_master: true } },
+        { name: "pull and rebase onto origin/master", value: { perform_pull: true, perform_migrations: false, perform_rebase_onto_origin_master: true } },
+        { name: "rebase onto origin/master only", value: { perform_pull: false, perform_migrations: false, perform_rebase_onto_origin_master: true } },
+        { name: "run migrations only", value: { perform_pull: false, perform_migrations: true, perform_rebase_onto_origin_master: false } }
+    ], per_page: 10)
+end
+
+def ask_user_if_should_run_main_update_sh_script()
+    PROMPT.yes?('do you want to run ./update_server_only_plus_migrations.sh?', value: 'n')
 end
 
 def remove_remote_prefix(branch_name)
@@ -103,6 +105,10 @@ def smart_checkout(branch_name, post_checkout_operations)
         run_system_command_with_colored_output("git pull") if post_checkout_operations[:perform_pull]
         run_system_command_with_colored_output("./migrator.py migrate", 'migrations') if post_checkout_operations[:perform_migrations]
         run_system_command_with_colored_output("git rebase origin/master") if post_checkout_operations[:perform_rebase_onto_origin_master]
+        if post_checkout_operations[:should_run_update_sh]
+            run_system_command_with_colored_output("./scripts/update_server_only_plus_migrations.sh")
+            run_system_command_with_colored_output("git reset --hard HEAD") # we don't need the shit left over update.sh
+        end
     ensure
         if uncomitted_changes_exist
             run_system_command_with_colored_output('git stash pop')
